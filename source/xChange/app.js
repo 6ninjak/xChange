74dcccd3127c0b24bcfcf1cf248df83b7e407633
6ninjak <colizzicoin.1794538@studenti.uniroma1.it>
2020-05-17 16:04:45 +0200

    migliori funzioni ausiliarie per couchdb

--- source/xChange/app.js
+++ source/xChange/app.js
@@ -7,22 +7,23 @@ const fs = require('fs');
 const db = nano.db.use('xchange');
 
 
+function errHandler(err, res) {
+    if (err) console.log(err);
+    console.log(res);
+}
+
 //funzioni helper per couchdb
-function updateDoc(id, info) {
-    db.get(id).then((res) => {
-        db.insert(Object.assign({ _id: id, _rev: res._rev }, info)).then((body) => {
-            console.log(body);
-        });
+db.insertOrUpdate = (docData, docId, callback) => {
+    db.get(docId, (err, res) => {
+        if (!err) docData._rev = res._rev;
+        db.insert(docData, docId, callback);
     });
 }
 
-function updateAttachToDoc(id, path, fileName, contentType) {
-    fs.readFile(path, (err, data) => {
+db.addAttachment = (docId, filePath, fileName, contentType, callback) => {
+    fs.readFile(filePath, (err, data) => {
         if (!err) {
-            const res = db.get(id);
-            db.attachment.insert(id, fileName, data, contentType, { rev: res._rev }).then((body) => {
-                console.log(body);
-            });
+            db.attachment.insert(docId, fileName, data, contentType, callback);
         }
     });
 }
@@ -53,18 +54,15 @@ app.use((req, res, next) => {
 
 // percorso di test per accesso a db
 app.get('/test', (req, res) => {
-    // db.insert({ tipo: 'user', username: 'alfredo', email: 'alfredo@pippo.com', password: 'inutile' }, 'alfredo@pippo.com');
-    // db.insert({ tipo: 'user', username: 'piero', email: 'pierpaolo@gugol.com', password: 'difficile' }, 'pierpaolo@gugol.com');
-    // db.insert({ tipo: 'user', username: 'alfredo', email: 'alfredo5@pippo.com', password: 'domani' }, 'alfredo5@pippo.com');
-    // db.insert({ tipo: 'scambio', _id: '12345a9876', user1: 'alfredo@pippo.com', user2: 'pierpaolo@gugol.com', oggetto1: 'ak47', oggetto2: 'un bottone' });
-    db.find({ 
-        selector: {
-            user1: 'alfredo@pippo.com'
-        },
-        fields: [ '_id' ]
-    }).then( (docs) => {
-        res.send(docs);
-    })
+    db.insertOrUpdate({ tipo: 'user', username: 'alfredo', email: 'alfredo@pippo.com', password: 'inutile' }, 'alfredo@pippo.com', errHandler);
+    db.insertOrUpdate({ tipo: 'user', username: 'piero', email: 'pierpaolo@gugol.com', password: 'difficile' }, 'pierpaolo@gugol.com', errHandler);
+    db.insertOrUpdate({ tipo: 'user', username: 'alfredo', email: 'alfredo4@pippo.com', password: 'domani' }, 'alfredo4@pippo.com', errHandler);
+    db.insertOrUpdate({ tipo: 'scambio', user1: 'alfredo@pippo.com', user2: 'pierpaolo@gugol.com', oggetto1: 'ak47', oggetto2: 'un bottone' }, '12345a9876', errHandler);
+    db.list((err, body) => {
+        if (!err) {
+            res.send(body.rows);
+        }
+    });
 })
 
 // get su / mostra home.html
