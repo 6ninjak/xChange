d442a58be5610200ae94984a998b723804b6cad4
6ninjak <colizzicoin.1794538@studenti.uniroma1.it>
2020-05-26 16:31:40 +0200

    formattazione e alcuni percorsi modificati

--- source/xChange/routes/users.js
+++ source/xChange/routes/users.js
@@ -5,7 +5,7 @@ const db = nano.db.use('xchange');
 
 // percorso di prova /users
 router.get('/', (req, res) => {
-    res.cookie('user', 'giacomo').send('percorso standard...');
+    res.send('percorso standard...');
 });
 
 router.post('/', (req, res) => {
@@ -15,7 +15,7 @@ router.post('/', (req, res) => {
         nome: body.nome,
         cognome: body.cognome,
         email: body.email,
-        pasword: body.password     
+        pasword: body.password
     }, body.email, (err, response) => {
         if (err && err.error == 'conflict') {
             res.render('registrazione', {
@@ -33,53 +33,6 @@ router.post('/', (req, res) => {
         }
     });
 });
-/*
-router.get('/profilo_esterno', (req, res) => {
-    var body =req.body
-    db.get
-});
-*/
-/*
-router.get('/home',(req, res) => {
-    var body = req.body;
-    db.fetch({nome:body})
-        .then(data => {
-        console.log(data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-});
-*/
-
-/*
-router.get('/home', (req, res) => {
-    var body = res.body;
-    db.get({
-        tipo: 'user',
-        dati: {
-            nome: body.nome,
-            cognome: body.cognome,
-            email: body.email,
-            pasword: body.password
-            }
-        },
-        body.nome==req.body, (err, response) => {
-            if (err){
-                res.render('home', {
-                    title: 'xChange - ricerca',
-                    error: err
-                });
-            }
-            else {
-                console.log(response);
-                res.redirect('/ricerca');
-            }
-        });
-    });
-*/
-
-
 
 // get su /users/:id conduce a profilo.html di :id
 router.get('/:id', (req, res) => {
@@ -93,11 +46,10 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// get su /users/:id/edit_dati conduce a edit_dati.html di :id
-router.get('/:id/edit_dati', (req, res) => {
+// get su /users/:id/edit conduce a edit_dati.html di :id
+router.get('/:id/edit', (req, res) => {
     res.render('edit_dati');
 });
-
 
 // post su users/:id aggiorna i dati da db e reindirizza a /users/:id
 router.post('/:id', (req, res) => {
