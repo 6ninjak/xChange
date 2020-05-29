f95607ea902078559f691d7a3d1689253d32123e
6ninjak <colizzicoin.1794538@studenti.uniroma1.it>
2020-05-30 01:43:22 +0200

    i percorsi sono tutti pronti, manca qualche funzionalità commentata nelle funzioni

--- source/xChange/routes/users.js
+++ source/xChange/routes/users.js
@@ -71,8 +71,8 @@ router.get('/:id/edit', (req, res) => {
     });
 });
 
-// post su users/:id aggiorna i dati da db e reindirizza a /users/:id
-router.post('/:id', (req, res) => {
+// put su users/:id aggiorna i dati da db e reindirizza a /users/:id
+router.put('/:id', (req, res) => {
     db.get(req.params.id, (err, doc) => {
         if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
             // inserisci dati su db
@@ -82,6 +82,68 @@ router.post('/:id', (req, res) => {
         } else res.redirect('/invalid');
     });
 });
+
+// questa put serve ad aggiornare l'immagine profilo dell'utente id
+// un utente può aggiornare solo la propria pagina profilo
+router.put('/:id/image', (req, res) => {
+    db.get(req.params.id, (err, doc) => {
+        if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
+            // inserisci file su db
+            res.redirect('.');
+        } else if (!err) {
+            res.send('Non puoi accedere a questa pagina, non fare il furbo!');
+        } else res.redirect('/invalid');
+    });
+})
+
+// questa get serve ad ottenere tutte le richieste di scambio ricevute non rifiutate
+// un utente può accedere solo alle richieste fatte a lui
+// le richieste vengono visualizzate in ordine dal più recente al meno recente (meglio al contrario?)
+// in caso di errori di parametri si viene reindirizzati alla pagina di errore
+router.get('/:id/ricevute', (req, res) => {
+    var q = {
+        selector: {
+            tipo: "scambio",
+            richiesto: req.params.id,
+            stato: { $ne: 'rifiutato' }
+        },
+        sort: [{ data: "desc"}],
+        fields: ["richiedente", "competenza", "messaggio", "stato"]
+    };
+    db.get(req.params.id, (err, doc) => {
+        if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
+            db.find(q, (err, body) => {
+                if (!err) res.json(body);
+                console.log(body);
+            });
+        } else if (!err) res.send('Non puoi accedere a questa pagina, non fare il furbo!');
+        else res.redirect('/invalid');
+    });
+})
+
+// questa get serve ad ottenere tutte le richieste di scambio effettuate
+// un utente può accedere solo alle richieste fatte da lui
+// le richieste vengono visualizzate in ordine dal più recente al meno recente (meglio al contrario?)
+// in caso di errori di parametri si viene reindirizzati alla pagina di errore
+router.get('/:id/effettuate', (req, res) => {
+    var q = {
+        selector: {
+            tipo: "scambio",
+            richiedente: req.params.id,
+        },
+        sort: [{ data: "desc"}],
+        fields: ["richiesto", "competenza", "messaggio", "stato"]
+    };
+    db.get(req.params.id, (err, doc) => {
+        if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
+            db.find(q, (err, body) => {
+                if (!err) res.json(body);
+                console.log(body);
+            });
+        } else if (!err) res.send('Non puoi accedere a questa pagina, non fare il furbo!');
+        else res.redirect('/invalid');
+    });
+})
 
 // questa post serve a fare richieste di scambio con l'utente con username id
 // la form deve contenere due campi: "competenza", "messaggio"
@@ -128,7 +190,7 @@ router.post('/:id/scambi', (req, res) => {
 // l'id di scambio fornito nella richiesta deve essere corretto (deve esistere uno scambio in attesa con lo stesso id)
 // la richiesta deve passare un parametro: "stato"
 // il parametro di stato può essere solo "accettato" o "rifiutato"
-// viene inserito un nuovo documento per lo scambio che include nell'id il timestamp di conferma
+// viene inserito un nuovo documento per lo scambio con le informazioni di contatto che include nell'id il timestamp di conferma
 // viene eliminato il documento con la richiesta in attesa, rendendo possibile effettuare un'altra richiesta
 router.put('/:id_user/scambio/:id_scambio', (req, res) => {
     let arrayScambio = req.params.id_scambio.split(':');
@@ -138,7 +200,7 @@ router.put('/:id_user/scambio/:id_scambio', (req, res) => {
         res.send('parametro di stato non presente o scorretto!');
     else {
         db.get(req.params.id_user, (err, doc) => {
-            if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password && doc.username == array_scambio[0]) {
+            if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password && doc.username == arrayScambio[0]) {
                 db.get(req.params.id_scambio, (err, document) => {
                     if (err) res.send("l'id di scambio fornito non è corretto");
                     else {
@@ -146,7 +208,8 @@ router.put('/:id_user/scambio/:id_scambio', (req, res) => {
                         let rev = document._rev;
                         delete document._rev;
                         delete document._id;
-                        // aggiungere campo dei contatti da fornire al richiedente
+                        document.info = 'email: ' + doc.email;
+                        // aggiungere campo del telefono da fornire
                         db.insert(document, req.params.id_scambio + ':' + Date.now(), (err, response) => {
                             if (!err) {
                                 console.log(response);
@@ -163,49 +226,6 @@ router.put('/:id_user/scambio/:id_scambio', (req, res) => {
             } else res.redirect('/invalid');
         });
     }
-})
-
-// questa post serve a pubblicare recensioni. 
-// deve essere fornito nell'url come query l'id dello scambio associato, nella forma "?scambio=<id_scambio>"
-// la richiesta deve avere tre parametri: "sintesi", "recensione", "voto"
-// se qualche parametro non è corretto si riceve un messaggio di errore. 
-// Un utente non può recensire se stesso
-// se una recensione con il medesimo id esiste già si riceve un messaggio di errore
-// l'id di una recensione è semplicemente l'id dello scambio associato combinato al tag "recensione"
-router.post('/:id/recensioni', (req, res) => {
-    db.get(req.params.id, (err, doc) => {
-        if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
-            res.send('Non puoi accedere a questa pagina, non fare il furbo!');
-        } else if (!err) {
-            if (req.body.voto > 5 || req.body.voto < 0) res.send('voto non valido');
-            else {
-                var documento = {
-                    tipo: 'recensione',
-
-                    recensito: req.params.id,
-                    recensore: req.cookies.cookieUtente.username,
-                    sintesi: req.body.sintesi,
-                    recensione: req.body.recensione,
-                    voto: req.body.voto,
-                    data: Date.now()
-                }
-                db.get(req.query.scambio, (err, doc) => {
-                    if (!err && doc.stato == "accettato")
-                        db.insert(documento, req.query.scambio + ':recensione', (err, response) => {
-                            if (err && err.error == 'conflict') {
-                                res.send('esiste già una recensione con questo id')
-                            } else {
-                                console.log(response);
-                                res.json(documento);
-                                // aggiornare media, punti e recensioni del utente recensito
-                            }
-                        });
-                    else if (!err) res.send('lo stato dello scambio non è valido');
-                    else res.send('id dello scambio errato');
-                });
-            }
-        } else res.redirect('/invalid');
-    });
 })
 
 // questa get serve ad ottenere le 6 recensioni più recenti fatte all'utente con username id
@@ -228,6 +248,59 @@ router.get('/:id/recensioni', (req, res) => {
                 if (!err) res.json(body);
                 console.log(body);
             });
+        } else res.redirect('/invalid');
+    });
+})
+
+// questa post serve a pubblicare recensioni. 
+// deve essere fornito nell'url come query l'id dello scambio associato, nella forma "?scambio=<id_scambio>"
+// la richiesta deve avere tre parametri: "sintesi", "recensione", "voto"
+// se qualche parametro non è corretto si riceve un messaggio di errore. 
+// Un utente non può recensire se stesso
+// se una recensione con il medesimo id esiste già si riceve un messaggio di errore
+// l'id di una recensione è semplicemente l'id dello scambio associato combinato al tag "recensione"
+// una volta pubblicata la recnsione vengono aggiornati i dati i punteggio dell'utente recensito
+router.post('/:id/recensioni', (req, res) => {
+    db.get(req.params.id, (err, doc) => {
+        if (!err && req.cookies.cookieUtente.username == doc.username && req.cookies.cookieUtente.password == doc.password) {
+            res.send('Non puoi accedere a questa pagina, non fare il furbo!');
+        } else if (!err) {
+            if (req.body.voto > 5 || req.body.voto < 0) res.send('voto non valido');
+            else {
+                var documento = {
+                    tipo: 'recensione',
+
+                    recensito: req.params.id,
+                    recensore: req.cookies.cookieUtente.username,
+                    sintesi: req.body.sintesi,
+                    recensione: req.body.recensione,
+                    voto: req.body.voto,
+                    data: Date.now()
+                }
+                db.get(req.query.scambio, (err, document) => {
+                    if (!err && document.stato == "accettato")
+                        db.insert(documento, req.query.scambio + ':recensione', (err, response) => {
+                            if (err && err.error == 'conflict') {
+                                res.send('esiste già una recensione con questo id')
+                            } else {
+                                console.log(response);
+                                res.json(documento);
+                                let new_punti = doc.punti + documento.voto;
+                                let new_recensioni = doc.recensioni + 1;
+                                let new_media = new_punti/new_recensioni;
+                                db.updateFields({
+                                    punti: new_punti,
+                                    recensioni: new_recensioni,
+                                    media : new_media,
+                                }, doc.username, (err, res) => {
+                                    if (!err) console.log(res)
+                                });
+                            }
+                        });
+                    else if (!err) res.send('lo stato dello scambio non è valido');
+                    else res.send('id dello scambio errato');
+                });
+            }
         } else res.redirect('/invalid');
     });
 })
