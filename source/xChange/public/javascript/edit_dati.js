ee36eb02d43efd3745b829b917d16cfc1b8a2985
6ninjak <colizzicoin.1794538@studenti.uniroma1.it>
2020-06-01 15:42:25 +0200

    ultimi cambiamenti

--- source/xChange/public/javascript/edit_dati.js
+++ source/xChange/public/javascript/edit_dati.js
@@ -1,9 +1,15 @@
 // controlla se i valori passati dalla form del file edit_dati.html sono giusti
 function verificaForm(){
-    if(document.getElementById("telefono").value.length < 9){
+    if(document.getElementById("telefono").value.length < 9 && document.getElementById("telefono").value.length != 0){
         alert("Il numero di telefono è troppo corto");
         return false;
     }  
-    alert("successo")  
-    return true
+    else {
+        // alert($("[data-id*='competenze]"));
+        // document.getElementById("professione").value = document.getElementById("professione").title
+        // alert(document.getElementById("professione").value);
+        // alert(document.getElementById("competenze").value);
+        alert("modifiche apportate con successo!")  
+        return true;
+    }
 }
